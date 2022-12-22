8a6ded6f5359ea8691d86f3c8c7f89411b8ef88e
APdu77 <apdu77@yahoo.fr>
2022-12-22 11:54:29 +0100

    beforeExpress

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-const esprex = require('./core/esprex')
+const esprex = require('./core/esprex');
 const app = esprex();
 
 app.get('/', (req, res) => {
-    res.end('Home Page');
+    res.end(`
+      <h1>Home Page</h1>
+      <form method="POST" action="/contact">
+        <input type="text" name="email">
+        <button type="submit">Go !</button>
+      </form>
+    `);
 });
 
 app.get('/contact', (req, res) => {
     res.end('Contact Page');
 });
 
+app.post('/contact', (req, res) => {
+    res.end('Contact Page (POST)');
+});
 
+app.get('/services', (req, res) => {
+    res.end('Services Page');
+});
 
+console.log(app.router.routes);
 
 module.exports = app;
